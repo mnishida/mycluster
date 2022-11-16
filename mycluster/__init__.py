51b98f47aaf093b57bf3e3f1f209f971aa82de49
mnishida <mnishida@hiroshima-u.ac.jp>
2022-11-17 08:55:51 +0900

    change expect

--- mycluster/__init__.py
+++ mycluster/__init__.py
@@ -209,7 +209,7 @@ class Cluster():
         if hasattr(self, 'pcluster'):
             self.pcluster.sendintr()
             # self.pcluster.expect(r"Removing pid file:")
-            self.pcluster.expect(r"engine set stopped", timeout=30)
+            self.pcluster.expect(r"Stopping engine(s)", timeout=30)
             delattr(self, 'pcluster')
         print("done.")
 
